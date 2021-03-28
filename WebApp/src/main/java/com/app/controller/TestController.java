52bbcd814dc888f3eb2eb4cbfbd274ff4c4dedbe
delahozric <delahozric@gmail.com>
2021-03-28 14:58:19 -0400

    Controller, stylesheet, and index page for debugging.

--- WebApp/src/main/java/com/app/controller/TestController.java
+++ WebApp/src/main/java/com/app/controller/TestController.java
@@ -1,13 +1,18 @@
 package com.app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HomeController {
+public class TestController {
 
 	@RequestMapping("/")
-	public String getHomePage() {
+	public String getHomePage(Model model) {
+		model.addAttribute("title", "TEST");
+		model.addAttribute("message", "DUMMY TEXT!");
 		return "index";
 	}
+
+
 }
