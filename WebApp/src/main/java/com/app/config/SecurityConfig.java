4c39f378a7105cb5c580cf4765e72263ee013e85
delahozric <delahozric@gmail.com>
2021-04-04 17:38:20 -0400

    Implemented configurations for spring security. Added error handling controller that writes to log.

---
+++ WebApp/src/main/java/com/app/config/SecurityConfig.java
@@ -0,0 +1,66 @@
+package com.app.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+@Configuration
+@EnableWebSecurity
+@ComponentScan("com.app")
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	private final UserDetailsService userDetailsService;
+	private final AccessDeniedHandler accessDeniedHandler;
+
+	@Autowired
+	public SecurityConfig(UserDetailsService userDetailsService, AccessDeniedHandler accessDeniedHandler) {
+		this.userDetailsService = userDetailsService;
+		this.accessDeniedHandler = accessDeniedHandler;
+	}
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+	}
+
+	public void configure(WebSecurity web) {
+		web
+				.ignoring()
+				.antMatchers("/js/**", "/img/**", "/css/**", "/resources/**", "/scripts/**");
+	}
+
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				.antMatchers("/").permitAll()
+				.antMatchers("/register").permitAll()
+				.antMatchers("/password-recovery").permitAll()
+				.antMatchers("/contactus").permitAll()
+				.antMatchers("/welcome").hasAnyAuthority("ADMIN", "USER")
+				.antMatchers("/ticket-center/**").hasAnyAuthority("ADMIN")
+				.anyRequest().authenticated()
+				.and()
+				.formLogin().loginPage("/login")
+				.loginProcessingUrl("/loginAction")
+				.usernameParameter("username").passwordParameter("password")
+				.defaultSuccessUrl("/welcome", true).permitAll()
+				.and()
+				.logout().logoutSuccessUrl("/").permitAll()
+				.and()
+				.exceptionHandling().accessDeniedHandler(accessDeniedHandler)
+				.and()
+				.csrf().disable();
+	}
+}
---
+++ WebApp/src/main/java/com/app/error/MyAccessDeniedHandler.java
@@ -0,0 +1,36 @@
+package com.app.error;
+
+import org.apache.log4j.Logger;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class MyAccessDeniedHandler implements AccessDeniedHandler {
+
+    private static final Logger logger = Logger.getLogger(AccessDeniedHandler.class);
+
+    @Override
+    public void handle(HttpServletRequest httpServletRequest,
+                       HttpServletResponse httpServletResponse,
+                       AccessDeniedException e) throws IOException {
+
+        Authentication auth
+                = SecurityContextHolder.getContext().getAuthentication();
+
+        if (auth != null) {
+            String logDetails = String.format("User '%s' attempted to access URL: %s",
+                    auth.getName(), httpServletRequest.getRequestURI());
+            logger.info(logDetails);
+        }
+
+        httpServletResponse.sendRedirect(httpServletRequest.getContextPath() + "/403");
+
+    }
+}
