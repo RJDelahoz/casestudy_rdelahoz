ee698db020479571ba61456673aee6b99e5dae86
delahozric <delahozric@gmail.com>
2021-04-07 11:46:25 -0400

    New webpages added to spring security. Added thymeleaf template dialect for spring security.

--- WebApp/src/main/java/com/app/config/WebConfig.java
+++ WebApp/src/main/java/com/app/config/WebConfig.java
@@ -10,6 +10,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
@@ -26,7 +27,7 @@ public class WebConfig implements WebMvcConfigurer {
 		templateResolver.setPrefix("/templates/");
 		templateResolver.setSuffix(".html");
 		templateResolver.setTemplateMode("HTML5");
-		templateResolver.setCacheable(false); // updated resources w/o redeploy
+		templateResolver.setCacheable(false);
 
 		return templateResolver;
 	}
@@ -34,8 +35,11 @@ public class WebConfig implements WebMvcConfigurer {
 	@Bean
 	public SpringTemplateEngine templateEngine() {
 		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+
 		templateEngine.setTemplateResolver(templateResolver());
+		templateEngine.addDialect(new SpringSecurityDialect());
 		templateEngine.setEnableSpringELCompiler(true);
+
 		return templateEngine;
 	}
 
